55b40c1cfd3650a3802c46daaa6bb0d6885088df
theCodeSculptor <50812798+theCodeSculptor@users.noreply.github.com>
2019-08-19 09:45:11 -0400

    Create index.js

---
+++ index.js
@@ -0,0 +1,113 @@
+const STORE = [
+    { 
+      question: 'How many teams are in the NBA league?',
+      answers: [
+        '16',
+        '29',
+        '30',
+        '18'
+      ],
+      correctAnswer: '30',
+    },
+    {
+      question: 'What is the Finals records of Michael Jordan during his career?',
+      answers: [
+      '8 wins 1 lose',
+      '5 wins 2 lose',
+      '3 wins 6 lose',
+      '6 wins 0 lose'
+      ],
+      correctAnswer: '6 wins 0 lose'
+    },
+    {
+      question: 'What is the highest score did Michael Jordan get in a single game?', 
+      answers: [
+        '56',
+        '81',
+        '69',
+        '63'
+      ],
+      correctAnswer: '3-6'
+    },
+    {
+      question:'Who got the highest score in a single game during NBA History?',
+      answers: [
+       'Michael Jordan',
+       'Kareem Abdul-Jabbar',
+       'Kobe Bryant',
+       'Wilt Chamberlain' 
+      ],
+      correctAnswer: 'Wilt Chamberlain'
+    },
+    {
+      question:'Which NBA teams won an NBA Championship from 1999-2002 & 2008-2010?',
+      answers: [
+       'Chicago Bulls',
+       'LA Lakers',
+       'Detroit Pistons',
+       'New York Knicks'
+      ],
+      correctAnswer: 'LA Lakers'
+    },
+    {
+      question:'How many points did Kobe Bryant score on the Toronto Raptors on January 22, 2006?',
+      answers: [
+       '10 Points',
+       '65 Points',
+       '81 Points',
+       '100 Points'
+      ],
+      correctAnswer: '81 Points'
+    },
+    {
+      question:'In 1996 NBA Draft, which overall pick was Kobe Bryant selected at?',
+      answers: [
+       '1st',
+       '3rd',
+       '8th',
+       '13th'
+      ],
+      correctAnswer: '13th'
+    },
+    {
+      question:'Which player won the 2007-2008 Finals MVP?',
+      answers: [
+       'Paul Pierce',
+       'Kevin Garnett',
+       'Pau Gasol',
+       'Kobe Bryant'
+      ],
+      correctAnswer: 'Paul Pierce'
+    },
+    {
+     question:'How many minutes are in each quarter of an NBA basketball game?',
+      answers: [
+       '10 minutes',
+       '12 minutes',
+       '8 minutes',
+       '15 minutes'
+      ],
+      correctAnswer: '12 minutes'
+    },
+    {
+     question:'Which NBA team won most Championships in NBA history?',
+      answers: [
+       'LA Lakers',
+       'Chicago Bulls',
+       'Boston Celtics',
+       'Golden State Warriors'
+      ],
+      correctAnswer: 'Boston Celtics'
+    },
+
+function generateQuestion () {
+
+}
+
+function startQuiz () {
+
+}
+
+function renderQuestion () {
+  
+}
